d4326c05943dbb3c16dcd78338cbbc74c058e6f1
Ameer Hamza <ah18r@fsu.edu>
2023-10-03 14:11:10 -0400

    Added publication info, and created single template
    Added all required information for publications;
    Created a base.html that is template for the website, and all other
    pages should extend this template;
    Extended publication.html using the template;
    Updated current designation;
    
    Signed-off-by: Ameer Hamza <ah18r@fsu.edu>

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from os import path
 import yaml
 
 app = Flask(__name__)
@@ -8,31 +9,46 @@ def read_yaml(file_path):
         data = yaml.safe_load(file);
     return data;
 
-basic = read_yaml('data/basic.yml');
+basic_path = path.join(app.root_path, 'data', 'basic.yml')
+_basic = read_yaml(basic_path);
 
 @app.route('/')
-def index():
-    return render_template('index.html', data=basic);
+def index_process():
+    return render_template('index.html', basic=_basic);
 
 @app.route('/publications')
-def publications():
-    return render_template('publications.html', data=basic);
+def publications_process():
+    publications_path = path.join(app.root_path, 'data', 'publications.yml')
+    _publications = read_yaml(publications_path)['publications'];
+    for publication in _publications:
+        authors = publication['authors'];
+        authors_citation = '';
+        for index, author in enumerate(authors):
+            author_name = author.split(' ');
+            if (index == publication['your_position']):
+                author_citation = '<b>' + author_name[0][0] + '. ' + author_name[1] + '</b>, ';
+            else:
+                author_citation = author_name[0][0] + '. ' + author_name[1] + ', ';
+            authors_citation += author_citation;
+        authors_citation = authors_citation[:-2];
+        publication['citation'] = authors_citation + ', "' + publication['title'] + '" in ' + publication['venue'] + ', ' + str(publication['year']) + '.';
+    return render_template('publications.html', publications=_publications, basic=_basic);
 
 @app.route('/projects')
-def projects():
-    return render_template('projects.html', data=basic);
+def projects_process():
+    return render_template('projects.html', basic=_basic);
 
 @app.route('/contact')
-def contact():
-    return render_template('contact.html', data=basic);
+def contact_process():
+    return render_template('contact.html', basic=_basic);
 
 @app.route('/cv')
-def cv():
-    return render_template('cv.html', data=basic);
+def cv_process():
+    return render_template('cv.html', basic=_basic);
 
 @app.route('/experience')
-def experience():
-    return render_template('experience.html', data=basic);
+def experience_process():
+    return render_template('experience.html', basic=_basic);
 
 if __name__ == '__main__':
     app.run(debug=True);
